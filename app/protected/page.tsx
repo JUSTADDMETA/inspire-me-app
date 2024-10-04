9977c5404ececbf41526464a171d55cb86c7a066
Leonhard Kleinfeld <104496345+ogchectoco@users.noreply.github.com>
2024-10-04 13:37:59 +0200

    vercel fix

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -30,7 +30,6 @@ export default async function ProtectedPage() {
         <UploadPage />
         <AdminView />
         <UserManagement />
-        <VideoCompression />
       </div>
     </div>
   );
