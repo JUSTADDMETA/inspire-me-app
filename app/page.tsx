4af11f0919907186219d007bdf4431a03f371ce0
Leonhard Kleinfeld <104496345+ogchectoco@users.noreply.github.com>
2024-10-04 19:33:50 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ const MemoizedSliderPage = React.memo(SliderPage);
 
 export default async function Index() {
   return (
-    <div className="flex justify-center items-center h-fit pt-32 pb-64">
+    <div className="flex justify-center items-center h-fit pt-4 md:pt-24 md:pb-24 lg:pt-32 lg:pb-64">
       <div className="w-full max-w-xs md:max-w-2xl lg:max-w-5xl overflow-hidden">
         <Suspense fallback={<div>Loading Slider...</div>}>
           <MemoizedSliderPage />
