1f7043d3a99a1873479f61d152bbdda866893ab4
Leonhard Kleinfeld <104496345+ogchectoco@users.noreply.github.com>
2024-10-07 15:36:26 +0200

    Update SliderPage.tsx

--- components/SliderPage.tsx
+++ components/SliderPage.tsx
@@ -23,7 +23,7 @@ type Video = {
 };
 
 const styles = {
-  container: "flex flex-col w-full h-full text-white h-screen",
+  container: "flex flex-col w-full h-full text-white h-fit md:h-screen",
   gridContainer: "grid w-full justify-center gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 pt-0 md:pt-8",
   leftColumn: "hidden md:flex content-start flex-wrap gap-2 flex-row md:col-span-2 lg:col-span-1 p-0 lg:p-4 h-full",
   middleColumn: "relative",
